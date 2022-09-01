a0b7c5ecedb9853f849d7b1fe9e0f5bb0413e172
emilio <emilio.tanowe.maddalena@gmail.com>
2022-09-01 10:46:10 +0200

    first commit

---
+++ ex1/main.m
@@ -0,0 +1,159 @@
+clear all
+close all
+clc
+
+rng(3)
+
+a = 0.8; b = 8; c = 0.8;
+f = @(z1,z2) 1 - a.*(z1.^2) + z2 + b*sin(c.*z2);
+
+z_min = [-10; -10];
+z_max = [10; 10];
+nz = 2;
+
+lengthscale = 5;
+kernel = @(x1,x2) exp(-dist(x1,x2').^2 / (2*lengthscale^2));
+
+Gamma = 1200;
+
+d = 100;
+Z = collect_data(d, z_min, z_max, 'rnd');
+
+fZ = f(Z(:,1),Z(:,2));
+
+delta_bar = 1;
+del = rand(d,1)*2*delta_bar - delta_bar;
+
+y = fZ + del;
+
+%% 3D results
+
+z_gran = [0.8 0.8];
+[ZZ1, ZZ2] = meshgrid(z_min(1):z_gran(1):z_max(2),z_min(2):z_gran(2):z_max(2));
+ZZ = [ZZ1(:) ZZ2(:)];
+
+fZZ = f(ZZ1(:), ZZ2(:));
+
+for i = 1:size(ZZ,1)
+    [ub, lb] = opt_bnd(ZZ(i,:), [Z y], kernel, Gamma, delta_bar);
+    UB(i) = ub; LB(i) = lb;
+    if mod(i,10) == 0, disp(i); end
+end
+disp('Done optimal!')
+
+for i = 1:size(ZZ,1)
+    [ub, lb] = subopt_bnd(ZZ(i,:), [Z y], kernel, Gamma, delta_bar);
+    SUB(i) = ub; SLB(i) = lb;
+    if mod(i,10) == 0, disp(i); end
+end
+disp('Done suboptimal!')
+
+%%
+close all
+
+sett = 'rnd';
+
+surf(ZZ1,ZZ2,reshape(UB,size(ZZ1)), 'FaceColor', '#0072BD' ,'FaceAlpha', 0.3, 'EdgeColor', 'none'); hold on 
+surf(ZZ1,ZZ2,reshape(fZZ,size(ZZ1)), 'FaceColor', 'k', 'FaceAlpha', 0.3, 'EdgeColor', 'none')
+
+xlim([-10 10]); ylim([-10 10]); zlim([-100 50]);
+xticks(-10:5:10); yticks(-10:5:10); zticks(-100:50:50);
+set(gcf,'Color','w','Position',[100 100 360 250]);
+
+if strcmp(sett, 'rnd')
+    title('Random sampling,           ','FontName', 'Serif', 'FontSize',16); 
+    text(-8,22.4,31,['$\bar \delta = ' num2str(delta_bar) '$'],'fontsize',16,'Interpreter','latex')
+elseif strcmp(sett, 'grid')
+    title('Grid sampling,           ','FontName', 'Serif', 'FontSize',16); 
+    text(-8,19.8,38,['$\bar \delta = ' num2str(delta_bar) '$'],'fontsize',16,'Interpreter','latex')
+end
+
+xl = xlabel('$z_1$','Interpreter','latex'); yl = ylabel('$z_2$','Interpreter','latex'); zl = zlabel('$f(z_1,z_2)$','Interpreter','latex');
+xl.Position(1) = xl.Position(1) + abs(xl.Position(1) * 0.1); 
+xl.Position(2) = xl.Position(2) + abs(xl.Position(2) * 0.15);
+yl.Position(1) = yl.Position(1) + abs(yl.Position(1) * 1.5); 
+yl.Position(2) = yl.Position(2) + abs(yl.Position(2) * 6.5);
+yl.Position(3) = yl.Position(3) - abs(yl.Position(3) * 0.1);
+zl.Position(1) = zl.Position(1) - abs(zl.Position(1) * 1.4); 
+zl.Position(2) = zl.Position(2) + abs(zl.Position(2) * 0.2);
+zl.Position(3) = zl.Position(3) - abs(zl.Position(3) * 4);
+
+set(gca,'FontWeight','normal',...
+        'FontSize', 16,... 
+        'FontName','Serif',...
+        'TitleFontWeight','normal',...
+        'TitleFontSize',1,...
+        'view',[32,23]);
+
+set(gca, 'Position', [0.16,0.124,0.66,0.78])
+    
+exportgraphics(gcf, ['ex1_' sett '_' num2str(delta_bar) '.pdf'], 'ContentType', 'vector')
+
+%% 2D results
+
+slice = -3;
+
+ZZ2 = linspace(z_min(2),z_max(2),100);
+ZZ1 = slice*ones(size(ZZ2));
+ZZ = [ZZ1(:) ZZ2(:)];
+
+fZZ = f(ZZ1(:), ZZ2(:));
+
+for i = 1:size(ZZ,1)
+    [ub, lb] = opt_bnd(ZZ(i,:), [Z y], kernel, Gamma, delta_bar);
+    UB(i) = ub; LB(i) = lb;
+    if mod(i,10) == 0, disp(i); end
+end
+disp('Done optimal!')
+
+for i = 1:size(ZZ,1)
+    [ub, lb] = subopt_bnd(ZZ(i,:), [Z y], kernel, Gamma, delta_bar);
+    SUB(i) = ub; SLB(i) = lb;
+    if mod(i,10) == 0, disp(i); end
+end
+disp('Done suboptimal!')
+
+%%
+close all
+
+sett = 'rnd';
+
+figure
+
+xx = ZZ2(ZZ1(:) == slice);
+gt = fZZ(ZZ1(:) == slice);
+ub = UB(ZZ1(:) == slice);
+lb = LB(ZZ1(:) == slice);
+sub = SUB(ZZ1(:) == slice);
+slb = SLB(ZZ1(:) == slice);
+
+fill([xx fliplr(xx)], [ub fliplr(sub)], 'k', 'FaceColor', 'g', 'EdgeColor', 'g', 'facealpha', 0.15, 'linewidth', 1); hold on
+fill([xx fliplr(xx)], [slb fliplr(lb)], 'k', 'FaceColor', 'g', 'EdgeColor', 'g', 'facealpha', 0.15, 'linewidth', 1); hold on
+fill([xx fliplr(xx)], [lb fliplr(ub)], 'k', 'FaceColor', '#0072BD', 'EdgeColor', '#0072BD', 'facealpha', 0.15, 'linewidth', 1);
+plot(xx,gt, 'k--', 'linewidth', 1.8); hold on;
+
+legend('sub-optimal','', 'optimal', 'AutoUpdate', 'off','NumColumns',2,'location','southeast')
+
+grid on
+xlim([-10 10]); ylim([-60 40]);
+xticks(-10:5:10); yticks(-60:20:40);
+xl = xlabel('$z_2$','Interpreter','latex'); yl = ylabel('$f(z_1,z_2)$','Interpreter','latex'); 
+xl.Position(2) = xl.Position(2) - abs(xl.Position(2) * 0.04); 
+set(gcf,'Color','w','Position',[100 100 350 250]);
+
+set(gca,'FontWeight','normal',...
+        'FontSize', 16,... 
+        'FontName','Serif',...
+        'TitleFontWeight','normal',...
+        'TitleFontSize',1,...
+        'Position', [0.166,0.152,0.73,0.75]);
+    
+if strcmp(sett, 'rnd')
+    title('Random sampling,           ','FontName', 'Serif', 'FontSize',16); 
+    text(3.3,45,'$\bar \delta = 1$','fontsize',16,'Interpreter','latex')
+elseif strcmp(sett, 'grid')
+    title('Grid sampling,           ','FontName', 'Serif', 'FontSize',16); 
+    text(2.4,45,'$\bar \delta = 1$','fontsize',16,'Interpreter','latex')
+end
+
+%exportgraphics(gcf, ['ex1_slice_' sett '_' num2str(delta_bar) '.pdf'], 'ContentType', 'vector')
---
+++ ex3/main.m
@@ -0,0 +1,131 @@
+clear all
+close all
+clc
+
+addpath('./utils')
+constants
+
+rng(1)
+global T_samp 
+
+kernel = @(x1,x2,lengthscale) exp(-dist(x1,x2').^2 / (2*lengthscale^2));
+dynamics = @(t,x,u) cstr(t,x,u);
+
+%% PART1
+
+% Gather a dataset to estimate the kernel hyperparams and the RKHS norms
+N = 8;
+D = 400*ones(1,N);
+dataset = collect_data(D, @(t,x,u) dynamics(t,x,u), x_min, x_max, u_min, u_max, 0, 'grid');
+
+% Estimate the kernel lengthscales
+lengthscale_range = 2:0.1:5;
+folds = 5;
+lambda = 0.0000001;
+[lengthscales, losses] = cv(folds, kernel, lambda, dataset, lengthscale_range);
+
+% Estimate ground-truths RKHS norms
+gammas = estimate_rkhs(dataset, @(x1,x2,lengthscale) kernel(x1,x2,lengthscale), lengthscales);
+
+%% PART2
+
+% Learn a 1-step ahead KRR to be the dynamics surrogate
+dataset = collect_data(400, @(t,x,u) dynamics(t,x,u), x_min, x_max, u_min, u_max, delta_bar, 'grid');
+
+for nx = 1:nx
+    
+    X{nx} = dataset{nx,1}(:,1:end-1);
+    y{nx} = dataset{nx,1}(:,end);
+
+    jitter = 1e-8;
+    n_data = size(X{nx},1);
+    K{nx} = kernel(X{nx},X{nx},lengthscales(nx,1)) + jitter*eye(n_data);
+    alpha{nx} = (K{nx} + n_data*lambda*eye(n_data))\y{nx}; 
+    
+    kernel_casadi = @(x1,x2,lengthscale) exp(-diag((x1-repmat(x2,size(x1,1),1))*(x1-repmat(x2,size(x1,1),1))') / (2*lengthscale^2));
+    krr{nx} = @(x) alpha{nx}'*kernel_casadi(X{nx},x,lengthscales(nx,1));
+
+end
+
+surrogate_model = @(z) [krr{1}(z); krr{2}(z)];
+disp([newline 'Done learning a surrogate model!'])
+
+%% PART 3
+
+% Compute an optimal control sequence
+% Choose an initial condition
+x0 = [1.23; 0.81]; 
+x0 = [1.365; 1.68];
+x0 = [2.64; 1.33]; 
+x0 = [2.7; 0.544]; 
+
+[x_opti, u_opti] = ocp(surrogate_model, x0, N);
+x_true = sim_sys_dt(dynamics,x_opti(:,1),u_opti,T_samp);
+
+plot_trajs(x_opti, x_true, xs, x_min, x_max, 1);
+disp([newline 'Done computing an optimal control sequence!'])
+
+%% PART 4
+
+% Collect data for uncertainty quantification 
+D = 800*ones(1,N);
+plts = false;
+datasets = collect_data(D, @(t,x,u) dynamics(t,x,u), x_min, x_max, u_min, u_max, delta_bar, 'ocp', plts, surrogate_model);
+
+% Verify how far apart the data are
+min_features_dists(datasets);
+min_queries_dists(datasets, x_opti, u_opti);
+
+%% PART 5
+
+% Augmentation factors for the norm and the noise bound
+delta_aug = 1.2; 
+gamma_aug = 1.2;
+
+disp([newline 'Computing the sub-optimal bounds:'])
+
+ubs_subopt = zeros(nx,N);
+lbs_subopt = zeros(nx,N);
+
+for step = 1:N    
+    for state = 1:nx
+    
+        data = datasets{state,step};
+        gamma = gammas(state,step);
+        z = [x_opti(:,1)' u_opti(1:step)];
+        
+        [ub, lb] = subopt_bnd(z, data, @(x1,x2) kernel(x1,x2,lengthscales(state,step)), gamma*gamma_aug, delta_bar*delta_aug);
+        ubs_subopt(state,step) = ub;
+        lbs_subopt(state,step) = lb;
+        
+    end
+    disp(['Step ' num2str(step) ' done...'])
+end
+
+plot_phase(x_opti, x_true, ubs_subopt, lbs_subopt, x_min, x_max, 2)
+plot_errbars(x_opti, x_true, ubs_subopt, lbs_subopt, x_min, x_max, 3);
+
+disp([newline 'Computing the optimal bounds:'])
+
+ubs_opt = zeros(nx,N);
+lbs_opt = zeros(nx,N);
+
+for step = 1:N
+    for state = 1:nx
+    
+        data = datasets{state,step};
+        gamma = gammas(state,step);
+        z = [x_opti(:,1)' u_opti(1:step)];
+        
+        [ub, lb] = opt_bnd(z, data, @(x1,x2) kernel(x1,x2,lengthscales(state,step)), gamma*gamma_aug, delta_bar*delta_aug);
+        ubs_opt(state,step) = ub;
+        lbs_opt(state,step) = lb;
+        
+    end
+    disp(['Step ' num2str(step) ' done...'])
+end
+
+plot_phase(x_opti, x_true, ubs_opt, lbs_opt, x_min, x_max, 5)
+plot_errbars(x_opti, x_true, ubs_opt, lbs_opt, x_min, x_max, 6)
+
+% EOF
